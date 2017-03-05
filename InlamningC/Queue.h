8a121742529f2f50edcf6510e77ed59f4a42a8cf
Capelliexp <the.1man@hotmail.com>
2017-03-05 17:34:45 +0100

    snart klar

--- InlamningC/Queue.h
+++ InlamningC/Queue.h
@@ -8,61 +8,111 @@
 template <typename T>
 class Queue : IQueue<T> {
 private:
-	T **itemArray;
+	T *itemArray;
 	int pos;
 	int arraySize;
 	int numberOfElements;
 public:
+	T GetItemArray(int i) const;
+	int GetPos() const;
+	int GetArraySize() const;
+	int GetNumberOfElements() const;
+
 	void Enqueue(const T& element);
 	T Dequeue() throw(...);
 	T Front() const throw(...);
 	bool IsEmpty() const;
+
 	void ExtendArray();
+	void PrintQueue() const;
 
 	Queue(Queue &otherQueueObject);
-	//Queue& operator=(Queue &otherQueueObject);
+	Queue& operator=(Queue &otherQueueObject);
 
-	Queue();
+	Queue(int start);
 	~Queue();
 };
 
 template <typename T>
-void Queue<T>::Enqueue(const T& element) {
-	T *elementValue = element;
+T Queue<T>::GetItemArray(int i) const {
+	return itemArray[i];
+}
 
+template <typename T>
+int Queue<T>::GetPos() const {
+	return pos;
+}
+
+template <typename T>
+int Queue<T>::GetArraySize() const {
+	return arraySize;
+}
+
+template <typename T>
+int Queue<T>::GetNumberOfElements() const {
+	return numberOfElements;
+}
+
+template <typename T>
+void Queue<T>::Enqueue(const T& element) {
 	if (numberOfElements == arraySize) {
 		ExtendArray();
-		itemArray[pos - 1] = elementValue;
+		int arrayPos1 = pos - 1;
+		if (arrayPos1 == -1) {
+			arrayPos1 = arraySize - 1;
+		}
+		itemArray[arrayPos1] = element;
 	}
 	else {
-		int arrayPos = pos + numberOfElements;
-		if (arrayPos > (arraySize - 1)) {
-			arrayPos -= arraySize;
+		int arrayPos2 = pos + numberOfElements;
+		if (arrayPos2 > (arraySize - 1)) {
+			arrayPos2 -= arraySize;
 		}
-		itemArray[arrayPos] = elementValue;
+		itemArray[arrayPos2] = element;
 	}
 	numberOfElements++;
 }
 
 template <typename T>
 T Queue<T>::Dequeue() throw(...) {
-	T returnItem = *itemArray[pos];
+	try {
+		if (numberOfElements > 0) {
+			T returnItem = itemArray[pos];
 
-	delete[] itemArray[pos];
-	numberOfElements--;
-	pos++;
-	if (pos > (arraySize - 1)) {
-		pos = 0;
+			itemArray[pos] = NULL;
+			numberOfElements--;
+			pos++;
+			if (pos > (arraySize - 1)) {
+				pos = 0;
+			}
+			return returnItem;
+		}
+		else {
+			pos = 0;
+			throw pos;
+		}
 	}
-
-	return returnItem;
+	catch (int e) {
+		std::cout << "Error: Dequeue of empty queue at pos " << e << std::endl;
+		return NULL;
+	}
 }
 
 template <typename T>
 T Queue<T>::Front() const throw(...) {
-	T returnItem = itemArray[pos];
-
-	return returnItem;
+	try {
+		if (numberOfElements != 0) {
+			T returnItem = itemArray[pos];
+			return returnItem;
+		}
+		else {
+			throw pos;
+		}
+	}
+	catch (int e) {
+		std::cout << "Error: Front of empty queue at pos " << e << std::endl;
+		return NULL;
+	}	
 }
 
 template <typename T>
@@ -75,20 +125,19 @@ bool Queue<T>::IsEmpty() const {
 
 template <typename T>
 void Queue<T>::ExtendArray() {
-	T** temp = new T*[arraySize];
+	T* temp = new T[arraySize];
 
 	for (int i = 0; i < arraySize; i++) {
-		temp[i] = itemArray[i];
+		temp[i] = this->itemArray[i];
 	}
 
-	delete[] itemArray;
-	this->itemArray = new T*[arraySize + 1];
+	this->itemArray = new T[arraySize + 1];
 
 	for (int i = 0; i < pos; i++) {
-		itemArray[i] = temp[i];
+		this->itemArray[i] = temp[i];
 	}
 	for (int i = (pos + 1); i < (arraySize + 1); i++) {
-		itemArray[i] = temp[i - 1];
+		this->itemArray[i] = temp[i - 1];
 	}
 
 	delete[] temp;
@@ -97,24 +146,60 @@ void Queue<T>::ExtendArray() {
 }
 
 template <typename T>
-Queue<T>::Queue(Queue &otherQueueObject) {}
+void Queue<T>::PrintQueue() const {
+	int i = pos;
+	int loopCount = 0;
 
-//template <typename T>
-//Queue& Queue<T>::operator=(Queue &otherQueueObject) {}
+	//std::cout << std::endl;
+	while (loopCount < numberOfElements) {
+		std::cout << "Element " << (loopCount + 1) << ": " << itemArray[i] << std::endl;
+		i++;
+		loopCount++;
+
+		if (i > (arraySize - 1)) {
+			i = 0;
+		}
+	}
+}
 
 template <typename T>
-Queue<T>::Queue() {
-	this->itemArray = new T*[0];
-	this->pos = 0:
-	this->arraySize = 0;
+Queue<T>::Queue(Queue &otherQueueObject) {
+	this->itemArray = new T[otherQueueObject.GetArraySize()];
+	this->pos = otherQueueObject.GetPos();
+	this->arraySize = otherQueueObject.GetArraySize();
+	this->numberOfElements = otherQueueObject.GetNumberOfElements();
+
+	for (int i = 0; i < arraySize; i++) {
+		this->itemArray[i] = otherQueueObject.GetItemArray(i);
+	}
+}
+
+template <typename T>
+Queue<T>& Queue<T>::operator=(Queue &otherQueueObject) {
+	this->itemArray = new T[otherQueueObject.GetArraySize()];
+	this->pos = otherQueueObject.GetPos();
+	this->arraySize = otherQueueObject.GetArraySize();
+	this->numberOfElements = otherQueueObject.GetNumberOfElements();
+
+	for (int i = 0; i < arraySize; i++) {
+		this->itemArray[i] = otherQueueObject.GetItemArray(i);
+	}
+	return *this;
+}
+
+template <typename T>
+Queue<T>::Queue(int start) {
+	this->itemArray = new T[start];
+	this->pos = 0;
+	this->arraySize = start;
 	this->numberOfElements = 0;
 }
 
 template <typename T>
 Queue<T>::~Queue() {
-	for (int i = 0; i < arraySize; i++) {
-		delete this->itemArray[i];
-	}
+	//for (int i = 0; i < arraySize; i++) {
+	//	delete itemArray[i];
+	//}
 	delete[] this->itemArray;
 }
 
--- InlamningC/Test.cpp
+++ InlamningC/Test.cpp
@@ -1,20 +1,162 @@
 #include "Queue.h"
+#include <exception>
 
 int main() {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	std::set_unexpected([]{
-		std::cout << "Unhandled exception" << std::endl;
-	});
-
-	Queue <int> Q;
+	Queue <int> Q(0);
 
 	Q.Enqueue(1);
 	Q.Enqueue(2);
 	Q.Enqueue(3);
-	Q.Enqueue(4);
-	Q.Enqueue(5);
 
 	getchar();
 	return 1;
 }
+
+/*int main() {	//the test
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	std::cout << "Press enter to start test 1 (with int)" << std::endl;
+	getchar();
+
+	std::cout << "create queue (Q1) of type int" << std::endl;
+	Queue <int> Q1(3);
+
+	std::cout << std::endl << "<add 5 items>" << std::endl;
+	Q1.Enqueue(1);	//add 1
+	Q1.Enqueue(2);	//add 2
+	Q1.Enqueue(3);	//add 3
+	Q1.Enqueue(4);	//add 4
+	Q1.Enqueue(5);	//add 5
+
+	std::cout << std::endl << "All items:" << std::endl;
+	Q1.PrintQueue();
+
+	//----
+
+	std::cout << std::endl << "<remove 2 items>" << std::endl;
+	Q1.Dequeue();	//remove 1
+	Q1.Dequeue();	//remove 2
+
+	std::cout << std::endl << "All items:" << std::endl;
+	Q1.PrintQueue();
+
+	std::cout << std::endl << "<copy queue to new queue-item (Q2)>" << std::endl;
+	Queue <int> Q2(Q1);
+
+	std::cout << std::endl << "All items of Q2:" << std::endl;
+	Q2.PrintQueue();
+
+	//----
+
+	std::cout << std::endl << "<add 2 items>" << std::endl;
+	Q1.Enqueue(11);	//add 11
+	Q1.Enqueue(12);	//add 12
+
+	std::cout << std::endl << "All items:" << std::endl;
+	Q1.PrintQueue();
+
+	std::cout << std::endl << "<copy Q1 to Q2>" << std::endl;
+	Q2 = Q1;
+
+	std::cout << std::endl << "All items of Q2:" << std::endl;
+	Q2.PrintQueue();
+
+	//----
+
+	std::cout << std::endl << "the front-most item is: " << Q1.Front() << std::endl;	//print front-most element (3)
+	std::cout << "is list empty? (0 = no, 1 = yes) " << Q1.IsEmpty() << std::endl;	//returns 0 (bcs list is not empty)
+
+	//----
+
+	std::cout << std::endl << "<remove 7 items> (5 items + 2 empty)" << std::endl;
+	Q1.Dequeue();	//remove 3
+	Q1.Dequeue();	//remove 4
+	Q1.Dequeue();	//remove 5
+	Q1.Dequeue();	//remove 11
+	Q1.Dequeue();	//remove 12
+	Q1.Dequeue();	//--
+	Q1.Dequeue();	//--
+
+	std::cout << std::endl << "All items:" << std::endl;
+	Q1.PrintQueue();
+
+	std::cout << std::endl << "is list empty? (0 = no, 1 = yes) " << Q1.IsEmpty() << std::endl;	//returns 1 (bcs list is empty)
+
+
+	//---------------------------------------------------
+
+	std::cout << std::endl << "Press enter to start test 2 (with char)";
+	getchar();
+
+	std::cout << std::endl << std::endl << "___________________________________________________" << std::endl << std::endl;
+
+	std::cout << std::endl << "create queue (Q3) of type char" << std::endl;
+	Queue <char> Q3(3);
+
+	std::cout << std::endl << "<add 5 items>" << std::endl;
+	Q3.Enqueue('a');	//add a
+	Q3.Enqueue('b');	//add b
+	Q3.Enqueue('c');	//add c
+	Q3.Enqueue('d');	//add d
+	Q3.Enqueue('e');	//add e
+
+	std::cout << std::endl << "All items:" << std::endl;
+	Q3.PrintQueue();
+
+	//----
+
+	std::cout << std::endl << "<remove 2 items>" << std::endl;
+	Q3.Dequeue();	//remove a
+	Q3.Dequeue();	//remove b
+
+	std::cout << std::endl << "All items:" << std::endl;
+	Q3.PrintQueue();
+
+	std::cout << std::endl << "<copy queue to new queue-item (Q4)>" << std::endl;
+	Queue <char> Q4(Q3);
+
+	std::cout << std::endl << "All items of Q4:" << std::endl;
+	Q4.PrintQueue();
+
+	//----
+
+	std::cout << std::endl << "<add 2 items>" << std::endl;
+	Q3.Enqueue('x');	//add x
+	Q3.Enqueue('y');	//add y
+
+	std::cout << std::endl << "All items:" << std::endl;
+	Q3.PrintQueue();
+
+	std::cout << std::endl << "<copy Q3 to Q4>" << std::endl;
+	Q4 = Q3;
+
+	std::cout << std::endl << "All items of Q4:" << std::endl;
+	Q4.PrintQueue();
+
+	//----
+
+	std::cout << std::endl << "the front-most item is: " << Q3.Front() << std::endl;	//print front-most element (c)
+	std::cout << "is list empty? (0 = no, 1 = yes) " << Q3.IsEmpty() << std::endl;	//returns 0 (bcs list is not empty)
+
+	//----
+
+	std::cout << std::endl << "<remove 7 items> (5 items + 2 empty)" << std::endl;
+	Q3.Dequeue();	//remove c
+	Q3.Dequeue();	//remove d
+	Q3.Dequeue();	//remove e
+	Q3.Dequeue();	//remove x
+	Q3.Dequeue();	//remove y
+	Q3.Dequeue();	//--
+	Q3.Dequeue();	//--
+
+	std::cout << std::endl << "All items:" << std::endl;
+	Q3.PrintQueue();
+
+	std::cout << std::endl << "is list empty? (0 = no, 1 = yes) " << Q3.IsEmpty() << std::endl;	//returns 1 (bcs list is empty)
+
+	std::cout << std::endl << "Press enter to exit" << std::endl;
+	getchar();
+	return 1;
+}*/
